3af2656793da29aaf546e713a3cf8531eb8dc385
Den Williams <work@denwilliams.net>
2020-04-27 15:11:21 +1000

    update usvc

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 #!/usr/bin/env node
 
-const got = require('got');
-const mqttusvc = require('mqtt-usvc');
+const got = require("got");
+const mqttusvc = require("mqtt-usvc");
 
-const service = mqttusvc.create();
+async function main() {
+  const service = await mqttusvc.create();
 
-const handler = () => {
-  got(service.config.observations_url, { json: true })
-  .then(res => {
-    const observation = res.body.observations.data[0];
+  const handler = () => {
+    got(service.config.observations_url, { json: true }).then(res => {
+      const observation = res.body.observations.data[0];
 
-    const current = {
-      cloud: observation.cloud,
-      temperature: observation.air_temp,
-      feelsLike: observation.apparent_t,
-      windSpeed: observation.wind_spd_kmh,
-      windDir: observation.wind_dir,
-      humidity: observation.rel_hum
-    };
+      const current = {
+        cloud: observation.cloud,
+        temperature: observation.air_temp,
+        feelsLike: observation.apparent_t,
+        windSpeed: observation.wind_spd_kmh,
+        windDir: observation.wind_dir,
+        humidity: observation.rel_hum
+      };
 
-    service.send('current', current, { retain: true });
-  });
-  // TODO: error handling
-};
+      service.send("current", current, { retain: true });
+    });
+    // TODO: error handling
+  };
 
-handler();
+  handler();
 
-// need to set this more intelligently
-setInterval(handler, 600000);
+  // need to set this more intelligently
+  setInterval(handler, 600000);
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
