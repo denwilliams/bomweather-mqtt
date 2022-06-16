267ed4d0444f002b820187b19c69d5e29b8192f8
Den Williams <dennis.williams@loke.com.au>
2022-06-16 22:28:11 +1000

    Fix incorrect docs, update deps

--- README.md
+++ README.md
@@ -2,21 +2,42 @@
 
 Publishes weather from Bureau of Meteorology Australia over MQTT.
 
-Checks every 10 minutes and publishes to `prefix/current`.
+Checks every 10 minutes (or configured interval) and publishes to `{prefix}/current`.
 
-## Example Config
-
-```
-mqtt:
-  uri: mqtt://localhost
-  prefix: bomweather/
-service:
-  latitude: -37.814
-  longitude: 144.96332
-```
+For example, given a configured prefix of `bomweather` an event will be emitted on `bomweather/current`.
 
 ## Running
+
+It is intended to be installed globally, ie `npm i -g bomweather-mqtt`
+
+Create a YAML file somewhere. See `config.example.yml`
+
+Run (replace path)
 
 ```
 CONFIG_PATH=/path/to/config.yml bomweather-mqtt
 ```
+
+You can also use Consul for config. See [mqtt-usvc](https://www.npmjs.com/package/mqtt-usvc) for more details.
+
+## Example Config
+
+```yml
+mqtt:
+  uri: mqtt://localhost
+  prefix: bomweather/
+service:
+  observations_url: "http://reg.bom.gov.au/fwo/IDV60901/IDV60901.95866.json"
+  poll_interval: 600000
+```
+
+poll_interval is optional.
+
+## Finding The Observation URL
+
+1. Go to http://www.bom.gov.au/catalogue/data-feeds.shtml
+2. Scroll down to "Observations - individual stations"
+3. Browse around until you reach an observation page. The URL will look something like this - http://reg.bom.gov.au/products/IDV60901/IDV60901.94866.shtml
+4. Find the "Other Formats" section on the page.
+5. Copy the URL for the JSON format, which will look something like - http://reg.bom.gov.au/fwo/IDV60901/IDV60901.94866.json
+
--- main.js
+++ main.js
@@ -7,36 +7,37 @@ const pkg = require("./package.json");
 async function main() {
   const service = await mqttusvc.create();
 
-  const handler = () => {
-    got(service.config.observations_url, { json: true })
-      .then((res) => {
-        const observation = res.body.observations.data[0];
+  const { observations_url, poll_interval = 600000 } = service.config;
 
-        const current = {
-          cloud: observation.cloud,
-          temperature: observation.air_temp,
-          feelsLike: observation.apparent_t,
-          windSpeed: observation.wind_spd_kmh,
-          windDir: observation.wind_dir,
-          humidity: observation.rel_hum,
-        };
+  const handler = async () => {
+    try {
+      const body = await got(observations_url).json();
+      const observation = body.observations.data[0];
 
-        service.send("~/current", current, { retain: true });
-      })
-      .catch((err) => {
-        console.error(`Weather fetch failed: ${err}`);
-      });
+      const current = {
+        cloud: observation.cloud,
+        temperature: observation.air_temp,
+        feelsLike: observation.apparent_t,
+        windSpeed: observation.wind_spd_kmh,
+        windDir: observation.wind_dir,
+        humidity: observation.rel_hum,
+      };
+
+      service.send("~/current", current, { retain: true });
+    } catch (err) {
+      console.error(`Weather fetch failed: ${err}`);
+    }
   };
 
   handler();
 
   // need to set this more intelligently, use last update time to know expected update time
-  setInterval(handler, 600000);
+  setInterval(handler, poll_interval);
 }
 
 main()
   .then(() => {
-    console.log(`Version ${pkg.version} started.`);
+    console.log(`${pkg.name} ${pkg.version} started.`);
   })
   .catch((err) => {
     console.error(err);
