bcd8d639a617fcfc18c78b1ae1ca8acaa0fa741a
Den Williams <dennis.williams@loke.com.au>
2023-07-22 21:15:04 +1000

    typo

--- sea.sh
+++ sea.sh
@@ -11,7 +11,7 @@ for os in linux-armv7l linux-arm64 # linux-x64 darwin-x64 darwin-arm64
 do
     download_url="https://nodejs.org/dist/v${nodejs_version}/node-v${nodejs_version}-${os}.tar.xz"
     curl -s "${download_url}" | tar -xJ -C "${NODE_DIR}"
-    cp "${NODE_DIR}/node-v${nodejs_version}-${os}/bin/node" ".sea/sensibo-mqtt-${os}"
+    cp "${NODE_DIR}/node-v${nodejs_version}-${os}/bin/node" ".sea/bomweather-mqtt-${os}"
     rm -rf "${NODE_DIR}/node-v${nodejs_version}-${os}"
-    npx postject ".sea/sensibo-mqtt-${os}" NODE_SEA_BLOB .sea/prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+    npx postject ".sea/bomweather-mqtt-${os}" NODE_SEA_BLOB .sea/prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 done
